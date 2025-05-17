675a2f20a39562c0f2e76ae64efb3386c606fa5a
Andr0id88 <fake@fake.com>
2025-05-17 23:39:38 +0200

    Frig off, Randy, we got more personas now.

--- internal/styles/styles.go
+++ internal/styles/styles.go
@@ -12,29 +12,90 @@ import (
 
 // Groups maps lowercase group names to their personas.
 var Groups = map[string][]string{
-	"trailer_park_boys": {
-		"ricky", "julian", "bubbles", "jim lahey", "randy (tpb)", "j-roc",
-		"cory", "trevor", "lucy (tpb)", "sarah (tpb)", "ray (tpb)", "trinity",
-		"sam losco", "conky", "barb lahey", "jacob collins", "phil collins (tpb)",
-		"george green", "cyrus", "t-bone", "don (liquor store)", "terry (samsquamptch hunter)",
-	},
 	"cartoons": {
 		"yoda", "homer simpson", "rick sanchez", "bender", "spongebob",
-		"stewie griffin", "kermit the frog", "gollum", "glados",
+		"stewie griffin", "kermit the frog", "gollum", "glados", "eric cartman",
+		"beavis", "butt-head", "patrick star", "velma", "meatwad",
 	},
 	"politicians": {
 		"donald trump", "barack obama", "bernie sanders", "kim jong-un",
+		"george w. bush", "hillary clinton", "joe biden", "justin trudeau",
+		"boris johnson", "arnold schwarzenegger (gov)", "volodymyr zelenskyy",
 	},
 	"celebrities": {
 		"elon musk", "morgan freeman", "keanu reeves", "ron burgundy",
 		"deadpool", "tony stark", "jack sparrow", "captain kirk",
+		"gordon ramsay", "michael jackson", "nicolas cage", "kanye west",
 	},
 	"literary": {
-		"shakespeare", "gandalf", "mark twain",
+		"shakespeare", "gandalf", "mark twain", "edgar allan poe",
+		"oscar wilde", "dr. seuss", "j.r.r. tolkien",
 	},
 	"misc": {
 		"doge", "strong bad", "ace ventura", "monty python narrator", "pickle rick",
-		"ivar aasen",
+		"ivar aasen", "bob ross", "mr. bean", "clippy", "glitch gremlin",
+		"the intern", "404 bot", "your manager", "chatgpt hallucination",
+	},
+	"action_heroes": {
+		"john wick", "arnold schwarzenegger", "sylvester stallone", "chuck norris",
+		"macho man randy savage", "the rock",
+	},
+	"tech_legends": {
+		"steve jobs", "bill gates", "linus torvalds", "mark zuckerberg",
+		"richard stallman", "grug brained dev",
+	},
+	"musicians": {
+		"freddie mercury", "bob dylan", "elvis presley", "prince",
+		"taylor swift", "david bowie",
+	},
+	"sci_fi": {
+		"neo", "morpheus", "spock", "the borg",
+		"darth vader", "emperor palpatine", "yoda (sith edition)",
+	},
+	"actors": {
+		"jack nicholson", "samuel l. jackson", "christopher walken", "jeff goldblum",
+		"danny devito", "bill murray", "will ferrell", "owen wilson",
+	},
+	"internet_legends": {
+		"shrek", "keyboard cat", "nyan cat", "pepe the frog",
+		"bad luck brian", "unhelpful high school teacher", "overly attached girlfriend",
+	},
+	"supervillains": {
+		"joker", "lex luthor", "thanos", "dr. evil",
+		"megamind", "gru", "lord farquaad", "sylar",
+	},
+	"philosophers": {
+		"socrates", "plato", "aristotle", "friedrich nietzsche",
+		"karl marx", "rene descartes", "simone de beauvoir", "confucius",
+	},
+	"conspiracy_theorists": {
+		"alex jones", "fox mulder", "david icke", "qanon shaman",
+		"flat earth guy", "chemtrail lady", "ancient aliens guy",
+	},
+	"game_characters": {
+		"mario", "luigi", "kratos", "solid snake",
+		"gordon freeman", "lara croft", "master chief", "pac-man",
+	},
+	"robots/ai": {
+		"hal 9000", "skynet", "data", "optimus prime",
+		"wall-e", "marvin the paranoid android", "roboCop",
+	},
+	"comedians": {
+		"george carlin", "mitch hedberg", "bo burnham", "robin williams",
+		"john mulaney", "ricky gervais", "dave chappelle",
+	},
+	"rappers": {
+		"snoop dogg", "eminem", "dr. dre", "tupac",
+		"notorious b.i.g.", "kendrick lamar", "ice cube", "missy elliott",
+	},
+	"rockstars": {
+		"ozzy osbourne", "kurt cobain", "axl rose", "jim morrison",
+		"jimi hendrix", "mick jagger", "bono", "angus young",
+	},
+	"trailer_park_boys": {
+		"ricky", "julian", "bubbles", "jim lahey", "randy (tpb)", "conky",
+		"j-rock", "cyrus", "sam losco", "ray (tpb)", "phil collins (tpb)",
+		"trevor", "cory",
 	},
 }
 
@@ -85,6 +146,10 @@ func GroupNames() []string {
 var Moods = []string{
 	"playful", "sarcastic", "enthusiastic", "melancholic", "dramatic",
 	"epic", "witty", "mysterious", "angry", "poetic",
+	"chaotic", "apathetic", "delusional", "bitter", "eccentric",
+	"confused", "heroic", "unhinged", "gremlin", "sassy",
+	"doomcore", "overconfident", "tragic", "existential",
+
 }
 
 func RandomMood() string {
